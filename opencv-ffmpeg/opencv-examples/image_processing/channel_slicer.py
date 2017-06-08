5da41e07cd0d7057a91d36acc9254b7728f38faa
Josh Romphf <joshr@Joshs-MacBook-Pro.local>
2017-06-07 23:00:17 -0400

    added setup.py for video_io

--- opencv-ffmpeg/opencv-examples/image_processing/channel_slicer.py
+++ opencv-ffmpeg/opencv-examples/image_processing/channel_slicer.py
@@ -1,11 +1,8 @@
-import sys
-sys.path.append('../')
-
-from video_io.io_parser import IOParser
+from io_parser import IOParser
 
 # video io
-from io import videoreader
-from io import videowriter
+from videoreader import VideoReader
+from videowriter import VideoWriter
 
 # OpenCV
 import cv2
@@ -14,18 +11,18 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    parser = io_parser.IOParser()
-    reader = videoreader.VideoReader(parser.input_file)
-    writer = videowriter.VideoWriter(parser.output_file)
+    parser = IOParser()
+    reader = VideoReader(parser.input_file)
+    writer = VideoWriter(parser.output_file, reader.fps, (reader.width, reader.height))
 
     # initialize an array WxHx1 to act as a placeholder for the other 2 channels
-    black_channel = np.zeros((reader.height, reader.width), dytpe=np.uint8) #unsigned 8-bit integers
+    black_channel = np.zeros((reader.height, reader.width), dtype=np.uint8) #unsigned 8-bit integers
 
     for frame in reader:
         # by default OpenCV arranges its matrices in BGR colorspace
         b, g, r = cv2.split(frame)
-        output_red = cv2.merge(black_channel, black_channel, r)
-        writer.write(output_red)
+        output_red = cv2.merge([black_channel, black_channel, r])
+        writer.write_frame(output_red)
 
 
 
---
+++ opencv-ffmpeg/opencv-examples/video_io/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='video_io',
+      version='0.1',
+      py_modules=['io_parser', 'videoreader', 'videowriter']
+      )
